12445c2560677fcb7a85e63081e24d9330db2a02
ProJamesEun <44720532+projameseun@users.noreply.github.com>
2024-05-28 20:12:57 +0900

    재귀함수
    피보나치,팩토리얼,꼬리재귀

--- 16.Recursion Function/16.Recursion Function/main.cpp
+++ 16.Recursion Function/16.Recursion Function/main.cpp
@@ -37,6 +37,65 @@ int Factorial_Re(int _Count)
 	return _Count * Factorial_Re(_Count - 1);
 }
 
+//피보나치 수열 
+int Fibonacci(int _Num)
+{
+	int iPreNum = 1;
+	int iPreNum2 = 1;
+
+	int iResult = 0;
+
+	if (_Num == 1 || _Num == 2)
+	{
+		return 1;
+	}
+
+	//_Num 3이상 
+	for (int i = 0; i < _Num -2; ++i)
+	{
+		iResult = iPreNum + iPreNum2;
+
+		//순서를 잘못하면 이상한 값이 들어가게 된다.
+		/*iPreNum2 = iResult;
+		iPreNum = iPreNum2;*/
+
+		iPreNum = iPreNum2;
+		iPreNum2 = iResult;
+		
+	}
+	
+	return iResult;
+}
+
+int Fibonacci_Re(int _Num)
+{
+	if (_Num == 1 || _Num == 2)
+	{
+		return 1;
+	}
+
+	return Fibonacci_Re(_Num - 1) + Fibonacci_Re(_Num - 2);
+
+
+}
+
+int FactorialTraill(int n , int acc)  // 
+{
+	if (n == 1)
+	{
+		return acc;
+	}
+
+	return FactorialTraill(n - 1, acc * n); //일반 재귀에서의 n * Fatorial(n - 1) 와 달리 반환값에서 추가 연산을 하지 않음
+}
+
+int FatorialTest(int n)
+{
+	return FactorialTraill(n, 1);
+}
+
+
+
 
 int main()
 {
@@ -67,9 +126,27 @@ int main()
 	std::cout << "Factorial_ReFunction : " << iValue << std::endl;
 
 
+	iValue = 0;
 	//피보나치해야됨
+	//1 1 2 3 5 8 13 21 34 55 89....
+	
+	
+
+	//피보나치해야됨
+	iValue = Fibonacci(5);
+
+	std::cout << "Fibonacci: " << iValue << std::endl;
 	
 	//꼬리재귀 설명잘한거 https ://bozeury.tistory.com/entry/%EA%BC%AC%EB%A6%AC-%EC%9E%AC%EA%B7%80-%EC%B5%9C%EC%A0%81%ED%99%94Tail-Recursion
+
+	iValue = Fibonacci_Re(5);
+
+	std::cout << "Fibonacci_Re: " << iValue << std::endl;
+
+	//꼬리재귀
+	iValue = FatorialTest(4);
+
+	std::cout << "FatorialTraill: " << iValue << std::endl;
 
 	return 0;
 }
