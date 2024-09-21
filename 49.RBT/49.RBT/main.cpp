c374f09df4038e0a25a6b1c1e7b796038d9477c1
ProJamesEun <44720532+projameseun@users.noreply.github.com>
2024-09-21 22:26:25 +0900

    RBT insert 만드는중
    RBT insert 만드는중

---
+++ 49.RBT/49.RBT/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "CBST.h"
+#include <map>
+
+
+int main()
+{
+
+
+	std::map<int, int> mapint;
+
+	mapint.insert(std::make_pair(100,100));
+
+	CBST<int, int> MyInt;
+
+	MyInt.insert(make_mypair(100, 0));
+	MyInt.insert(make_mypair(50, 0));
+	MyInt.insert(make_mypair(25, 0));
+	MyInt.insert(make_mypair(200, 0));
+
+
+	
+	return 0;
+}
