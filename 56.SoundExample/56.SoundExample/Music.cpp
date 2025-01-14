901d8371f981e9a433c6b824f49a531947d4f01b
ProJamesEun <44720532+projameseun@users.noreply.github.com>
2025-01-14 20:23:58 +0900

    sound기능
    sound기능

---
+++ 56.SoundExample/56.SoundExample/Music.cpp
@@ -0,0 +1,18 @@
+#include "Music.h"
+
+void Music::InitRecordName()
+{
+	//음악 레코드 추가
+	m_RecordName.push_back(L"TroyeSIvan");
+	m_RecordName.push_back(L"Maroon5");
+	m_RecordName.push_back(L"YouDaEn");
+	m_RecordName.push_back(L"AnnerMarie");
+	m_RecordName.push_back(L"JangBeomJun");
+	m_RecordName.push_back(L"GyungSeo");
+	m_RecordName.push_back(L"AllTimeLow");
+	m_RecordName.push_back(L"OnePiece");
+	m_RecordName.push_back(L"PostMalone");
+	m_RecordName.push_back(L"SungSiKyung");
+
+
+}
---
+++ 56.SoundExample/56.SoundExample/main.cpp
@@ -0,0 +1,16 @@
+#include "Info.h"
+#include "Music.h"
+
+int main()
+{
+	srand(static_cast<unsigned int>(time(NULL)));
+
+	Music mic;
+
+	mic.InitRecordName();
+	
+
+	//레코드의 경로추가 
+
+	return 0;
+}
